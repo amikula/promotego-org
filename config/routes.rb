6d8bd6b1c02fd341d1feaeda55a1f06661898fb7
Alf Mikula <alf@Basil.local>
2008-04-27 08:09:33 -0700

    Added restful_authentication plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resource :session
+
+  map.resources :users
+
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
