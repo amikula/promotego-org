8aaa155511ef930caf6a976817014253223f16ce
Alf Mikula <alf@Basil.local>
2008-05-11 21:33:55 -0700

    Brought location to 100% coverage

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -17,5 +17,32 @@ describe Location do
     @location.city="Washington"
     @location.state="DC"
     @location.geocode_address.should == "1600 Pennsylvania Ave., Washington, DC"
+
+    @location.errors.should be_empty
+  end
+
+  it "should report an error if at least city and state or zip code is not present" do
+    @location.street_address="1600 Pennsylvania Ave."
+
+    @location.geocode_address.should == nil
+    @location.errors.should_not be_empty
+  end
+
+  it "should geocode with city and state or zip only, if street address is not present" do
+    @location.city = "Washington"
+    @location.state = "DC"
+
+    @location.geocode_address.should == "Washington, DC"
+
+    @location.errors.should be_empty
+  end
+
+  it "should set an error if geocode is not successful" do
+    result = mock("geocode_result")
+    result.stub!(:success).and_return(false)
+    GeoKit::Geocoders::MultiGeocoder.stub!(:geocode).and_return(result)
+
+    @location.geocode.should == nil
+    @location.errors.should_not be_empty
   end
 end
