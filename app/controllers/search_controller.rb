bf6d871a5fb384430591ef34b7aaab7de3e5f382
Alf Mikula <alf@Basil.local>
2008-05-27 21:44:40 -0700

    Adding some more search tests for searching with type

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,16 +4,24 @@ class SearchController < ApplicationController
     @radius = params[:radius].to_d if params[:radius]
     if params[:type]
       @type = Type.find_by_name(params[:type].gsub(/_/, ' ').singularize.titleize)
+      unless @type
+        flash[:error] = "Type '#{params[:type]}' is invalid."
+        redirect_to :action => :radius
+        return
+      end
+
+      @type_id = @type.id
     elsif params[:type_id]
       # Todo turn this into a search for type by ID
       @type_id = params[:type_id].to_d if params[:type_id]
     end
+
     @radii = [1,5,10,25,50,100]
     @types = Type.find(:all)
     if(params[:location])
       @location.geocode
       find_params = {:origin => @location, :within => @radius}
-      find_params.merge!(:conditions => ['type_id = ?', @type.id]) if @type
+      find_params.merge!(:conditions => ['type_id = ?', @type_id]) if @type_id
       @results = Location.find(:all, find_params)
 
       flash.now[:error] = "No locations matched your search" if @results.blank?
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,6 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe SearchController do
+  before(:each) do
+    @params = {"street_address" => "169 N. Berkeley Ave.", "city" => "Pasadena",
+              "state" => "CA", "zip_code" => "91107"}
+  end
+
   it "should assign radii and types on radius_search" do
     get :radius
     assigns[:location]
@@ -9,53 +14,75 @@ describe SearchController do
   end
 
   it "should perform a search if location is provided" do
-    params = {"street_address" => "169 N. Berkeley Ave.", "city" => "Pasadena", "state" => "CA", "zip_code" => "91107"}
-    origin = Location.new(params)
-    Location.should_receive(:new).with(params).and_return(origin)
-    Location.should_receive(:find).with(:all, :origin => origin, :within => 5).and_return([])
-    get :radius, "location" => params, :radius => "5"
+    origin = Location.new(@params)
+    Location.should_receive(:new).with(@params).and_return(origin)
+    Location.should_receive(:find).with(:all, :origin => origin, :within => 5).
+      and_return([])
+    get :radius, "location" => @params, :radius => "5"
   end
 
   it "should retrieve expected locations on search" do
-    new_location(:name => "EarthLink Office", :street_address => "2947 Bradley St.", :city => "Pasadena", :state => "CA", :zip_code => "91107").geocode.save!
-    create_location(:name => "White House", :street_address => "1600 Pennsylvania Ave.", :city => "Washington", :state => "DC").geocode.save!
-    create_location(:name => "Mom and Dad's", :zip_code => "77072").geocode.save!
+    new_location(:name => "EarthLink Office",
+                 :street_address => "2947 Bradley St.", :city => "Pasadena",
+                 :state => "CA", :zip_code => "91107").geocode.save!
+    new_location(:name => "White House",
+                    :street_address => "1600 Pennsylvania Ave.",
+                    :city => "Washington", :state => "DC").geocode.save!
+    new_location(:name => "Mom and Dad's", :zip_code => "77072").geocode.save!
 
-    params = {"street_address" => "169 N. Berkeley Ave.", "city" => "Pasadena", "state" => "CA", "zip_code" => "91107"}
-    get :radius, "location" => params, :radius => "5"
+    get :radius, "location" => @params, :radius => "5"
 
     assigns[:results].size.should == 1
   end
 
   it "should set a message if no search results are present" do
-    params = {"street_address" => "169 N. Berkeley Ave.", "city" => "Pasadena", "state" => "CA", "zip_code" => "91107"}
-    origin = Location.new(params)
-    Location.should_receive(:new).with(params).and_return(origin)
-    Location.should_receive(:find).with(:all, :origin => origin, :within => 5).and_return([])
+    origin = Location.new(@params)
+    Location.should_receive(:new).with(@params).and_return(origin)
+    Location.should_receive(:find).with(:all, :origin => origin, :within => 5).
+      and_return([])
 
     # stub out sweep so we can read flash.now
     @controller.instance_eval{flash.stub!(:sweep)}
 
-    get :radius, "location" => params, :radius => "5"
+    get :radius, "location" => @params, :radius => "5"
 
     flash.now[:error].should == "No locations matched your search"
   end
 
   describe "with type" do
     it "should search only go clubs when go_clubs type is present" do
-      params = {"street_address" => "169 N. Berkeley Ave.", "city" => "Pasadena", "state" => "CA", "zip_code" => "91107"}
-      origin = Location.new(params)
+      origin = Location.new(@params)
 
       go_club = mock_model(Type, :name => "Go Club")
       Type.should_receive(:find_by_name).with("Go Club").and_return(go_club)
 
-      Location.should_receive(:new).with(params).and_return(origin)
-      Location.should_receive(:find).with(:all, :origin => origin, :within => 5, :conditions => ["type_id = ?", go_club.id]).and_return([])
+      Location.should_receive(:new).with(@params).and_return(origin)
+      Location.should_receive(:find).with(:all, :origin => origin, :within => 5,
+                                          :conditions =>
+                                  ["type_id = ?", go_club.id]).and_return([])
 
-      get :radius, "type" => "go_clubs", :radius => "5", "location" => params
+      get :radius, "type" => "go_clubs", :radius => "5", "location" => @params
     end
 
-    it 'should search only go clubs when go clubs is selected'
-    it 'should redirect with a message when type is invalid'
+    it 'should search only go clubs when go clubs is selected' do
+      origin = Location.new(@params)
+
+      go_club = mock_model(Type, :name => "Go Club")
+
+      Location.should_receive(:new).with(@params).and_return(origin)
+      Location.should_receive(:find).
+        with(:all, :origin => origin, :within => 5,
+             :conditions => ["type_id = ?", go_club.id]).and_return([])
+
+      get :radius, "type_id" => go_club.id, :radius => "5",
+        "location" => @params
+    end
+
+    it 'should redirect with a message when type is invalid' do
+      get :radius, "type" => "bogus_type", :radius => "5", "location" => @params
+
+      flash[:error].should == "Type 'bogus_type' is invalid."
+      response.should redirect_to(:action => "radius")
+    end
   end
 end
