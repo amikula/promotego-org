d143ce1dfeb54aebe63e15563d0c0b5e6673a739
Alf Mikula <alf@Basil.local>
2008-05-28 06:19:32 -0700

    Added hiding of type pulldown when type param is present, plus spec to check correct type is selected when type_id is present

--- spec/views/search/radius.html.erb_spec.rb
+++ spec/views/search/radius.html.erb_spec.rb
@@ -3,8 +3,50 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe "/search/radius.html.erb" do
   include SearchHelper
 
-  it 'should not display type selector when type parameter is present'
-  it 'should select current type when type_id parameter is present'
+  before(:each) do
+    @types = [mock_model(Type, :name => "Foo"),
+              mock_model(Type, :name => "Bar"),
+              mock_model(Type, :name => "Baz")]
+    assigns[:radii] = [1,2,3]
+    assigns[:types] = @types
+  end
+
+  it 'should display type selector when type parameter is not present' do
+    render "/search/radius.html.erb"
+
+    response.should have_tag("select[name=type]")
+  end
+
+  it 'should not display type selector when type parameter is present' do
+    params[:type] = :anything
+
+    render "/search/radius.html.erb"
+
+    response.should_not have_tag("select[name=type]")
+  end
+
+  it 'should select first type when @type_id matches first type' do
+    check_type_selected(@types[0])
+  end
+
+  it 'should select second type when @type_id matches second type' do
+    check_type_selected(@types[1])
+  end
+
+  it 'should select third type when @type_id matches third type' do
+    check_type_selected(@types[2])
+  end
+
+  def check_type_selected(type)
+    assigns[:type_id] = type.id
+    render "/search/radius.html.erb"
+
+    response.should have_tag("option[value=#{type.id}][selected=selected]")
+
+    @types.reject{|x| x == type}.each do |check_type|
+      response.should_not have_tag("option[value=#{check_type.id}][selected=selected]")
+    end
+  end
 end
 
 
