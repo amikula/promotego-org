8cd9f4b0afc02e13400a1820b2da189b2456cc3c
Alf Mikula <amikula@gmail.com>
2008-06-08 14:30:45 -0700

    Added role logic to user

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,9 +1,4 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
-  it "Add test for confirming that owners can create owners"
-  it "Add test for confirming that owners can create super-users"
-  it "Add test for confirming that super-users cannot create super-users"
-  it "Add test for confirming that super-users can create admins"
-  it "Add test for confirming that admins cannot create admins"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-# Be sure to include AuthenticatedTestHelper in spec/spec_helper.rb instead.
-# Then, you can remove it from this and the functional test.
-include AuthenticatedTestHelper
-
 describe User do
   fixtures :users
 
@@ -108,10 +104,106 @@ describe User do
     users(:quentin).remember_token_expires_at.between?(before, after).should be_true
   end
 
-  it "should have many locations" do
-    user = User.new
-    user.locations << Location.new
-    user.locations << Location.new
+  describe "has many" do
+    before(:each) do
+      @user = User.new
+    end
+
+    it "locations" do
+      @user.locations << Location.new
+      @user.locations << Location.new
+    end
+
+    it "user_roles" do
+      @user.user_roles << UserRole.new
+      @user.user_roles << UserRole.new
+    end
+
+    it "roles" do
+      @user.stub!(:id).and_return(1)
+      @user.stub!(:new_record?).and_return(false)
+      @user.roles << mock_model(Role)
+      @user.roles << mock_model(Role)
+    end
+  end
+
+  describe "has_role" do
+    before(:each) do
+      @user = User.new
+    end
+
+    it "should query roles by name, using the given symbol" do
+      @user.roles.should_receive(:find_by_name).with("administrator").
+        and_return(:administrator_role)
+      @user.has_role(:administrator).should_not be_false
+    end
+  end
+
+  describe "add_role" do
+    before(:each) do
+      @user = User.new
+      @granting_user = mock_model(User)
+    end
+
+    it "should take an argument of role and granting user" do
+      @user.add_role(:role, @granting_user)
+    end
+
+    it "should allow owners to create other owners" do
+      @granting_user.should_receive(:has_role).with(:owner).and_return(true)
+      user_should_add_role(:owner)
+
+      @user.add_role(:owner, @granting_user)
+    end
+
+    it "should raise an error when non-owners try to create owners" do
+      @granting_user.should_receive(:has_role).with(:owner).and_return(false)
+
+      lambda {@user.add_role(:owner, @granting_user)}.
+        should raise_error(SecurityError)
+    end
+
+    it "should allow owners to create super-users" do
+      @granting_user.should_receive(:has_role).with(:owner).and_return(true)
+      user_should_add_role(:super_user)
+
+      @user.add_role(:super_user, @granting_user)
+    end
+
+    it "should raise an error when non-owners try to create super-users" do
+      @granting_user.should_receive(:has_role).with(:owner).and_return(false)
+
+      lambda {@user.add_role(:super_user, @granting_user)}.
+        should raise_error(SecurityError)
+    end
+
+    it "should allow super-users to create administrators" do
+      @granting_user.should_receive(:has_role).with(:super_user).
+        and_return(true)
+      user_should_add_role(:administrator)
+
+      @user.add_role(:administrator, @granting_user)
+    end
+
+    it "should raise an error when non-super-users try to create other administrators" do
+      @granting_user.should_receive(:has_role).with(:super_user).
+        and_return(false)
+
+      lambda {@user.add_role(:administrator, @granting_user)}.
+        should raise_error(SecurityError)
+    end
+
+    def user_should_add_role(role_sym)
+      user_role = mock_model(UserRole)
+      UserRole.should_receive(:new).and_return(user_role)
+      user_role.should_receive(:granting_user=).with(@granting_user)
+
+      Role.should_receive(:find_by_name).with(role_sym.to_s).
+        and_return(:role)
+      user_role.should_receive(:role=).with(:role)
+
+      @user.user_roles.should_receive(:<<).with(user_role)
+    end
   end
 
 protected
