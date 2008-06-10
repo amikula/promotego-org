a67d6c910c1b0ffa556be69289d44b8013914c13
Alf Mikula <amikula@gmail.com>
2008-06-10 05:42:20 -0700

    Added 'other_users' style for other users' locations in locations/index

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,5 +1,4 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
-  it "administors should get other users' locations with a special style"
 end
--- spec/views/locations/index.html.erb_spec.rb
+++ spec/views/locations/index.html.erb_spec.rb
@@ -4,43 +4,72 @@ describe "/locations/index.html.erb" do
   include LocationsHelper
   
   before(:each) do
-    location_98 = mock_model(Location)
-    location_98.should_receive(:name).and_return("MyString")
-    location_98.should_receive(:type_id).and_return("1")
-    location_98.should_receive(:street_address).and_return("MyString")
-    location_98.should_receive(:city).and_return("MyString")
-    location_98.should_receive(:state).and_return("MyString")
-    location_98.should_receive(:zip_code).and_return("MyString")
-    location_98.should_receive(:phone_number).and_return("MyString")
-    location_98.should_receive(:hours).and_return("MyString")
-    location_98.should_receive(:lat).and_return("MyString")
-    location_98.should_receive(:lng).and_return("MyString")
-    location_99 = mock_model(Location)
-    location_99.should_receive(:name).and_return("MyString")
-    location_99.should_receive(:type_id).and_return("1")
-    location_99.should_receive(:street_address).and_return("MyString")
-    location_99.should_receive(:city).and_return("MyString")
-    location_99.should_receive(:state).and_return("MyString")
-    location_99.should_receive(:zip_code).and_return("MyString")
-    location_99.should_receive(:phone_number).and_return("MyString")
-    location_99.should_receive(:hours).and_return("MyString")
-    location_99.should_receive(:lat).and_return("MyString")
-    location_99.should_receive(:lng).and_return("MyString")
+    @location_98 = mock_model(Location)
+    @location_98.should_receive(:name).and_return("Location1 Name")
+    @location_98.should_receive(:type_id).and_return("123")
+    @location_98.should_receive(:street_address).and_return("street_address 1")
+    @location_98.should_receive(:city).and_return("city1")
+    @location_98.should_receive(:state).and_return("ST1")
+    @location_98.should_receive(:zip_code).and_return("zip1")
+    @location_98.should_receive(:phone_number).and_return("phnum1")
+    @location_98.should_receive(:hours).and_return("hours1")
+    @location_98.should_receive(:lat).and_return("lat1")
+    @location_98.should_receive(:lng).and_return("lng1")
+    @location_99 = mock_model(Location)
+    @location_99.should_receive(:name).and_return("Location2 Name")
+    @location_99.should_receive(:type_id).and_return("456")
+    @location_99.should_receive(:street_address).and_return("street_address 2")
+    @location_99.should_receive(:city).and_return("city2")
+    @location_99.should_receive(:state).and_return("ST2")
+    @location_99.should_receive(:zip_code).and_return("zip2")
+    @location_99.should_receive(:phone_number).and_return("phnum2")
+    @location_99.should_receive(:hours).and_return("hours2")
+    @location_99.should_receive(:lat).and_return("lat2")
+    @location_99.should_receive(:lng).and_return("lng2")
 
-    assigns[:locations] = [location_98, location_99]
+    @user = mock_model(User)
+    @other_user = mock_model(User)
+
+    @location_98.should_receive(:user).and_return(@user)
+    @location_99.should_receive(:user).and_return(@other_user)
+
+    @controller.stub!(:current_user).and_return(@user)
+
+    assigns[:locations] = [@location_98, @location_99]
   end
 
   it "should render list of locations" do
     render "/locations/index.html.erb"
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
+    response.should have_tag("tr>td", "Location1 Name")
+    response.should have_tag("tr>td", "Location2 Name")
+    response.should have_tag("tr>td", "123")
+    response.should have_tag("tr>td", "456")
+    response.should have_tag("tr>td", "street_address 1")
+    response.should have_tag("tr>td", "street_address 2")
+    response.should have_tag("tr>td", "city1")
+    response.should have_tag("tr>td", "city2")
+    response.should have_tag("tr>td", "ST1")
+    response.should have_tag("tr>td", "ST2")
+    response.should have_tag("tr>td", "zip1")
+    response.should have_tag("tr>td", "zip2")
+    response.should have_tag("tr>td", "phnum1")
+    response.should have_tag("tr>td", "phnum2")
+    response.should have_tag("tr>td", "hours1")
+    response.should have_tag("tr>td", "hours2")
+    response.should have_tag("tr>td", "lat1")
+    response.should have_tag("tr>td", "lat2")
+    response.should have_tag("tr>td", "lng1")
+    response.should have_tag("tr>td", "lng2")
+  end
+
+  it "should render other users' locations with class other_users" do
+    render "/locations/index.html.erb"
+
+    response.should have_tag("tr>td", "Location1 Name")
+
+    response.should have_tag("tr.other_users") do
+      with_tag("td", "Location2 Name")
+    end
   end
 end
 
