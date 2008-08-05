33c84eb5967e99fba086ba03cd1624ec8af140d5
Alf Mikula <alf@Basil.local>
2008-08-05 06:30:54 -0700

    Added detection of logged in user to remove login and register links

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -40,11 +40,28 @@ describe "/layouts/application.html.erb" do
     response.should have_tag("title")
   end
 
-  it "should have a link to log in" do
+  it "should have a link to log in if no user is logged in" do
     do_render
 
     response.should have_tag("a[href=/session/new]", "Log in")
   end
+
+  it "should display a register link if no user is logged in" do
+    do_render
+
+    response.should have_tag("a[href=/users/new]", "Register")
+  end
+
+  it "should not display a login or register link if a user is logged in" do
+    template.should_receive(:current_user).and_return(:current_user)
+
+    do_render
+
+    response.should_not have_tag("a[href=/users/new]", "Register")
+    response.should_not have_tag("a[href=/session/new]", "Log in")
+  end
+
+  it "should display the user name if a user is logged in"
 end
 
 
