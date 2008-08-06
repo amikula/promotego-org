717587fb2817a6d3d5c35008c566cd71892d8ec6
Alf Mikula <alf@Basil.local>
2008-08-05 21:41:57 -0700

    Added logout link, display of user login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :users
 
-  map.resource :session
+  map.resource :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
 
