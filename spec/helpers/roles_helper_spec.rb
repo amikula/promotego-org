355a34da8119a37551ccfc51f046459fe36ae016
Alf Mikula <alf@Basil.local>
2008-08-22 06:40:55 -0700

    Fixing rspec tests

--- spec/helpers/roles_helper_spec.rb
+++ spec/helpers/roles_helper_spec.rb
@@ -4,7 +4,7 @@ describe RolesHelper do
   
   #Delete this example and add some real ones or delete this file
   it "should include the RoleHelper" do
-    included_modules = self.metaclass.send :included_modules
+    included_modules = (class << helper; self; end).send :included_modules
     included_modules.should include(RolesHelper)
   end
   
--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -4,7 +4,7 @@ describe SearchHelper do
   
   #Delete this example and add some real ones or delete this file
   it "should include the SearchHelper" do
-    included_modules = self.metaclass.send :included_modules
+    included_modules = (class << helper; self; end).send :included_modules
     included_modules.should include(SearchHelper)
   end
   
