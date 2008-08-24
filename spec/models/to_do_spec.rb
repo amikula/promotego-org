cb98af9127068059ae87c5a552045dcafa733e62
Alf Mikula <alf@Basil.local>
2008-08-24 08:24:53 -0700

    Correcting edit field to allow administrators to change ownership of clubs

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -2,11 +2,10 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
   it "get site online"
-  it "test editing of clubs"
   it "edit new fields and contacts"
   it "improve display of email addresses on club page"
   it "email address obfuscator"
-  it "expand address recognition in ClubScraper ('1635 N Nash St..', suite #'s, etc.)"
+  it "expand address recognition in ClubScraper ('1635 N Nash St..', '(807 E 10th St)', suite #'s, etc.)"
   it "add AGA icon"
   it "add club name to title of page for SEO"
 end
