7c40cb3e80e9545e62487dd5661fdceb2095f970
Alf Mikula <amikula@gmail.com>
2008-08-24 08:09:24 -0700

    Adding recognition of mailto links with missing colon

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -2,10 +2,11 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
   it "get site online"
-  it "fix mailto in email addresses scraped from usgo.org"
   it "test editing of clubs"
   it "edit new fields and contacts"
   it "improve display of email addresses on club page"
   it "email address obfuscator"
   it "expand address recognition in ClubScraper ('1635 N Nash St..', suite #'s, etc.)"
+  it "add AGA icon"
+  it "add club name to title of page for SEO"
 end
