904a35598f899a2e9ab5b7eb19f41824074f87d9
Alf Mikula <amikula@gmail.com>
2008-08-25 19:53:45 -0700

    Added github wiki link

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
-  it "add github wiki link"
   it "email address obfuscator"
   it "expand address recognition in ClubScraper ('1635 N Nash St..', '(807 E 10th St)', suite #'s, etc.)"
   it "show closest club when none in radius"
