3d5e3c4ecbcf2bcc1dd20526c51e2a1ae3f96e6e
Alf Mikula <alf@Basil.local>
2008-08-25 19:00:23 -0700

    Updating todo file

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,15 +1,19 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
-  it "get site online"
-  it "edit contacts"
-  it "fix ownership editing"
   it "improve display of email addresses on club page"
+  it "add github wiki link"
   it "email address obfuscator"
   it "expand address recognition in ClubScraper ('1635 N Nash St..', '(807 E 10th St)', suite #'s, etc.)"
+  it "show closest club when none in radius"
+  it "show link to propose club when none in radius"
+  it "fix ownership editing"
+  it "edit contacts"
+  it "restyle, adding color and fonts, and moving contact and about to bottom"
   it "add AGA icon"
   it "add aga representative role"
   it "add editing of AGA status for aga rep only"
   it "add club name to title of page for SEO"
+  it "add club slug to url for SEO"
   it "add map to club"
 end
