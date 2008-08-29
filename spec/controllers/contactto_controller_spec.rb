d2d8dcdb1b717d3a3a4b6db7c64e04c00912321b
Alf Mikula <amikula@gmail.com>
2008-08-29 06:26:52 -0700

    Adding mail sending

--- spec/controllers/contactto_controller_spec.rb
+++ spec/controllers/contactto_controller_spec.rb
@@ -9,5 +9,24 @@ describe ContacttoController do
   end
 
   describe "GET 'send_mail'" do
+    it "should take the form params and send the email" do
+      encoded_email = Loopy::EmailObfuscator.obfuscate_email("email@domain.com")
+      Obfuscated.should_receive(:deliver_contact).
+        with("email@domain.com", "from_address",
+             "[PromoteGo] A message from a PromoteGo.org user", "message")
+
+      get 'send_mail', :email => encoded_email, :from => "from_address",
+        :message => "message"
+    end
+
+    it "should set a flash info message" do
+      Obfuscated.should_receive(:deliver_contact)
+
+      get 'send_mail', :email => ""
+
+      flash[:info].should_not be_nil
+    end
+
+    it "should redirect to home"
   end
 end
--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -2,9 +2,11 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
   it "graceful fallback of obfuscated email links"
+  it "doesn't allow form user to specify 'from' address"
+  it "automatically put cursor in search window"
   it "configure outgoing email for production"
   it "experiment with grouping clubs with city only"
-  it "expand address recognition in ClubScraper ('1635 N Nash St..', '(807 E 10th St)', suite #'s, etc.)"
+  it "expand address recognition in ClubScraper ('1635 N Nash St..', '(807 E 10th St)', '11772 Parklawn Drive, Rockville, MD 20852.', suite #'s, etc.)"
   it "show closest club when none in radius"
   it "show link to propose club when none in radius"
   it "fix ownership editing"
