fc18572164e6cd02e5110b0a5b15beb32d8a0bc2
Alf <alf.mikula@latimes.com>
2008-08-29 16:47:08 -0700

    Putting input cursor in search window

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,8 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
-  it "automatically put cursor in search window"
-  it "configure outgoing email for production"
   it "experiment with grouping clubs with city only"
   it "expand address recognition in ClubScraper ('1635 N Nash St..', '(807 E 10th St)', '11772 Parklawn Drive, Rockville, MD 20852.', suite #'s, etc.)"
   it "show closest club when none in radius"
