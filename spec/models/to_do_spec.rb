2e61c93ea277bc098d1febfb6d1da9c18a99277d
Alf Mikula <alf@Basil.local>
2008-09-11 05:52:58 -0700

    Added _contact_form partial

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "TODO:" do
   it "edit contacts"
+  it "fix display of no email"
   it "add club slug to url for SEO"
   it "improve phone number type parsing: 480-429-0300W, 480-831-5567 H, cell 626-297-0208, Cell: 303-618-0206, 720-331-3170 cell, 305-652-1137 (Voice mail only), Cell 717-682-2667, 717-898-7911 Ex22, 832-755-5330 cell, 281-599-0837 home"
   it "try adding address detection to contact parser"
@@ -13,6 +14,8 @@ describe "TODO:" do
   it "add RSS icon with link to club blog feed"
   it "add email icon with link to club mailing list"
   it "add text search"
-  it "add clickable U.S. map"
+  it "add Google map of all clubs"
+  it "add a sitemap to expose all known clubs"
+  it "support international addresses"
   it "add pretty error page"
 end
