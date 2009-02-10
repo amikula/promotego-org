3b04352f7d32ee31aa2dc61c7eb5c54537f0ac53
Alf Mikula <amikula@gmail.com>
2009-02-10 06:30:42 -0800

    Adding parent assignments to parent_id migration

--- db/migrate/20090208175449_add_parent_id_to_role.rb
+++ db/migrate/20090208175449_add_parent_id_to_role.rb
@@ -1,6 +1,14 @@
 class AddParentIdToRole < ActiveRecord::Migration
   def self.up
     add_column :roles, :parent_id, :integer
+
+    %w{owner super_user administrator}.inject(nil) do |parent,current|
+      role = Role.find_by_name(current)
+      role.parent = parent
+      role.save!
+
+      role
+    end
   end
 
   def self.down
