4b2063c7d3fe2edc6925e64f547d597f12fc336e
Alf Mikula <alf@Basil.local>
2009-02-14 17:42:57 -0800

    Added Affiliate and Affiliation models to keep track of which clubs are associated with which locations

---
+++ db/migrate/20090215011044_create_affiliates.rb
@@ -0,0 +1,15 @@
+class CreateAffiliates < ActiveRecord::Migration
+  def self.up
+    create_table :affiliates do |t|
+      t.string :name
+      t.string :logo_path
+      t.integer :admin_role_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :affiliates
+  end
+end
