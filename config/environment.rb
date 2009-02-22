80d7f62e6d68bb4e31c328953daad46dd820e236
Alf Mikula <amikula@gmail.com>
2009-02-21 18:00:12 -0800

    Removing git submodules for rspec and rails

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 ADMIN_EMAIL = "do-not-reply@promotego.org"
 
