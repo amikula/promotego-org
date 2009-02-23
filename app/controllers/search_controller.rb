2541e99d5bae2bb144f5f94da097c9266d662034
Alf Mikula <alf@Basil.local>
2009-02-23 06:15:46 -0800

    Adding logic to only find visible Locations on find_closest

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -62,9 +62,9 @@ class SearchController < ApplicationController
   def find_closest
     closest_params = {:origin => @address, :within => 100}
     if (@type_id && @type_id > 0)
-      closest_params[:conditions] = ['lat is not null and lng is not null and type_id = ?', @type_id]
+      closest_params[:conditions] = ['lat is not null and lng is not null and hidden = false and type_id = ?', @type_id]
     else
-      closest_params[:conditions] = 'lat is not null and lng is not null'
+      closest_params[:conditions] = 'lat is not null and lng is not null and hidden = false'
     end
 
     @closest = Location.find_closest(closest_params)
