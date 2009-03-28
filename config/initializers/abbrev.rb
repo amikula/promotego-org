e0c8e08faf484b53248c35c245365546924997e8
Alf Mikula <amikula@gmail.com>
2009-03-28 06:47:13 -0700

    Fixing ruby 1.8.6 Hash incompatibility problem

--- config/initializers/abbrev.rb
+++ config/initializers/abbrev.rb
@@ -1,4 +1,5 @@
 COUNTRY_FROM_ABBREV = YAML.load(File.new("db/country-codes.yml"))
 COUNTRY_TO_ABBREV = COUNTRY_FROM_ABBREV.invert
 STATE_FROM_ABBREV = YAML.load(File.new("db/states_provinces.yml"))
-STATE_TO_ABBREV = Hash[STATE_FROM_ABBREV.map{|key,value| [key, value.invert]}]
+STATE_TO_ABBREV = {}
+STATE_FROM_ABBREV.each_pair{|key,value| STATE_TO_ABBREV[key] = value.invert}
