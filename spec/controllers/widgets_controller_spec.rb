40fe6c4733f1cd51ec88dd368abcd2c7a8a3dc94
Alf Mikula <alf@Basil.local>
2009-05-10 08:20:15 -0700

    Initial work on adding search widget

---
+++ spec/controllers/widgets_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe WidgetsController do
+
+  #Delete this example and add some real ones
+  it "should use WidgetsController" do
+    controller.should be_an_instance_of(WidgetsController)
+  end
+
+end
