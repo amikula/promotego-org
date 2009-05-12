774eab52de282427cb97424ab8d769411b84e870
Alf Mikula <alf@Basil.local>
2009-05-12 06:19:00 -0700

    Configuring vlad for Passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,17 @@
 set :application, 'promotego-org'
-set :domain, 'promotego.org'
+set :domain, 'webapps@promotego.org'
 set :deploy_to, '/var/tmp/promotego-vlad'
 set :repository, 'git://github.com/amikula/promotego-org.git'
+set :app_command, "/usr/sbin/apache2ctl"
+
+namespace :vlad do
+  desc 'Restart Passenger'
+  remote_task :start_app, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  desc 'Restarts the apache servers'
+  remote_task :start_web, :roles => :app do
+    run "sudo #{app_command} restart"
+  end
+end
