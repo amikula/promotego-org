4a8da01183ee08f21a5b47a34a29e5e5472df21b
Alf Mikula <alf@Basil.local>
2009-05-17 12:14:06 -0700

    refining styles and making javascript independent of Prototype

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,11 +1,9 @@
 class WidgetsController < ApplicationController
-  layout false
-
   def search
     respond_to do |format|
       format.html
       format.js do
-        javascript_widget = @template.escape_javascript(render_to_string)
+        javascript_widget = @template.escape_javascript(render_to_string :partial => 'search_widget')
         render :text => "document.write('#{javascript_widget}')", :content_type => "text/javascript"
       end
     end
