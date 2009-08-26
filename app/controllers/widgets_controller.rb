c18fb6a0ac84818f6dc072fc19dfc67d25f31d1a
amikula <amikula@gmail.com>
2009-08-26 15:30:51 -0700

    Parenthesizing arguments for future version

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,7 +3,7 @@ class WidgetsController < ApplicationController
     respond_to do |format|
       format.html
       format.js do
-        javascript_widget = @template.escape_javascript(render_to_string :partial => 'search_widget')
+        javascript_widget = @template.escape_javascript(render_to_string(:partial => 'search_widget'))
         render :text => "document.write('#{javascript_widget}')", :content_type => "text/javascript"
       end
     end
