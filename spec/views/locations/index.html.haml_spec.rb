9db15ae4a7e10ef2ef56ae7ce0ec6392885bedae
Alf Mikula <alf@Basil.local>
2009-09-03 20:58:06 -0700

    Fixed specs, hiding header row for state when state is blank

--- spec/views/locations/index.html.haml_spec.rb
+++ spec/views/locations/index.html.haml_spec.rb
@@ -9,13 +9,13 @@ describe "/locations/index" do
     @type1 = mock_model(Type, :name => "Type1 Name")
     @location_98.should_receive(:city).and_return("city1")
     @location_98.should_receive(:state).and_return("ST1")
-    @location_98.should_receive(:country).and_return("Country1")
+    @location_98.should_receive(:street_address).at_least(:once).and_return("StreetAddress1")
     @location_99 = mock_model(Location)
     @location_99.should_receive(:name).any_number_of_times.and_return("Location2 Name")
     @type2 = mock_model(Type, :name => "Type2 Name")
     @location_99.should_receive(:city).and_return("city2")
     @location_99.should_receive(:state).and_return("ST2")
-    @location_99.should_receive(:country).and_return("Country2")
+    @location_99.should_receive(:street_address).at_least(:once).and_return("StreetAddress2")
 
     @user = mock_model(User)
     @other_user = mock_model(User)
@@ -37,8 +37,6 @@ describe "/locations/index" do
     response.should have_tag("tr>td", "city2")
     response.should have_tag("tr>td", "ST1")
     response.should have_tag("tr>td", "ST2")
-    response.should have_tag("tr>td", "Country1")
-    response.should have_tag("tr>td", "Country2")
   end
 
   it "should render other users' locations with class other_users" do
