97129cf7b96b1f381014508bc35c7275327248c1
Alf Mikula <amikula@gmail.com>
2009-10-03 21:50:14 -0700

    Updating to latest cucumber

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -18,7 +18,8 @@ config.action_mailer.delivery_method = :test
 config.action_mailer.default_url_options = {:host => "testhost",
                                             :only_path => false }
 
-config.gem 'cucumber',    :lib => false,        :version => '>=0.2.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
+config.gem 'cucumber',    :lib => false,        :version => '>=0.3.104' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
 config.gem 'webrat',      :lib => false,        :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
-config.gem 'rspec',       :lib => false,        :version => '>=1.2.6' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
-config.gem 'rspec-rails', :lib => 'spec/rails', :version => '>=1.2.6' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+config.gem 'rspec',       :lib => false,        :version => '>=1.2.8' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
+config.gem 'rspec-rails', :lib => false,        :version => '>=1.2.7.1' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -7,7 +7,6 @@ module NavigationHelpers
   #
   def path_to(page_name)
     case page_name
-
     when /the homepage/
       '/'
     when /the new account page/
