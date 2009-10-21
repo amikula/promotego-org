0cdd8b0ef3d357153f5fbd8e7c8b50c61d7528b7
Alf Mikula <amikula@gmail.com>
2009-10-21 06:06:30 -0700

    Moving focusing behavior to jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-//Event.addBehavior({
-//  "a.obfuscated": EmailDecoder
-//});
 $(document).ready(function() {
   $("a.obfuscated").each(decodeLink)
 });
