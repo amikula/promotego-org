ba6bdd7786fbbecb99bc21957b5485da2d82a05f
Alf Mikula <amikula@gmail.com>
2009-10-20 20:56:58 -0700

    Switching to jQuery.  Fixed email decoding.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-Event.addBehavior({
-  "a.obfuscated": EmailDecoder
+//Event.addBehavior({
+//  "a.obfuscated": EmailDecoder
+//});
+$(document).ready(function() {
+  $("a.obfuscated").each(decodeLink)
 });
--- public/javascripts/email_decoder.js
+++ public/javascripts/email_decoder.js
@@ -1,21 +1,42 @@
-EmailDecoder = Behavior.create({
-  initialize: function() {
-    var anchor = this.element;
-    var href = anchor.getAttribute('href');
-    var address = href.replace(/.*contactto\/new\/([a-z0-9._%$-]+)\^([a-z0-9._%$-]+)/i, '$1' + '@' + '$2').replace(/\$/g, '.');
-    
-    if (href != address) {
-      anchor.setAttribute('href', 'mailto:' + this.decode(address));
-    }
+function decodeLink() {
+  anchor = $(this)
+  var href = anchor.attr("href");
+  var address = href.replace(/.*contactto\/new\/([a-z0-9._%$-]+)\^([a-z0-9._%$-]+)/i, '$1' + '@' + '$2').replace(/\$/g, '.');
 
-    if (anchor.text.match(/^(email this contact|click here)$/i)) {
-      anchor.innerHTML = this.decode(address)
-    }
-  }, 
-  
-  decode: function(encoded) {
-    return encoded.replace(/[a-zA-Z]/g, function(c){
-        return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
-    });
+  if (href != address) {
+    anchor.attr('href', 'mailto:' + decode(address));
   }
-});
+
+  if (anchor.text().match(/^(email this contact|click here)$/i)) {
+    anchor.html(decode(address))
+  }
+}
+
+function decode(encoded) {
+  return encoded.replace(/[a-zA-Z]/g, function(c){
+      return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+  });
+}
+
+//EmailDecoder = Behavior.create({
+//  initialize: function(theEvent) {
+//    var anchor = theEvent.element;
+//    var href = anchor.attr('href');
+//    alert(href)
+//    var address = href.replace(/.*contactto\/new\/([a-z0-9._%$-]+)\^([a-z0-9._%$-]+)/i, '$1' + '@' + '$2').replace(/\$/g, '.');
+
+//    if (href != address) {
+//      anchor.setAttribute('href', 'mailto:' + this.decode(address));
+//    }
+
+//    if (anchor.text.match(/^(email this contact|click here)$/i)) {
+//      anchor.innerHTML = this.decode(address)
+//    }
+//  },
+
+//  decode: function(encoded) {
+//    return encoded.replace(/[a-zA-Z]/g, function(c){
+//        return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+//    });
+//  }
+//});
