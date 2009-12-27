6c4370901f09f4a88d32bb575f36943d984e7ac6
Alf Mikula <amikula@gmail.com>
2009-12-27 11:22:35 -0800

    Tests were not running outside of spork.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'spork'
 
+ENV["RAILS_ENV"] ||= 'test'
+
 Spork.prefork :env => 'test' do
   require File.expand_path('../config/environment', File.dirname(__FILE__))
   require 'spec/autorun'
@@ -18,43 +20,10 @@ Spork.prefork :env => 'test' do
     config.use_instantiated_fixtures  = false
     config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
-    # == Fixtures
-    #
-    # You can declare fixtures for each example_group like this:
-    #   describe "...." do
-    #     fixtures :table_a, :table_b
-    #
-    # Alternatively, if you prefer to declare them only once, you can
-    # do so right here. Just uncomment the next line and replace the fixture
-    # names with your fixtures.
-    #
-    # config.global_fixtures = :table_a, :table_b
-    #
-    # If you declare global fixtures, be aware that they will be declared
-    # for all of your examples, even those that don't use them.
-    #
-    # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-    #
-    # config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-    #
-    # == Mock Framework
-    #
-    # RSpec uses it's own mocking framework by default. If you prefer to
-    # use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-    #
-    # == Notes
-    #
-    # For more information take a look at Spec::Runner::Configuration and Spec::Runner
-
     config.include FixtureReplacement
   end
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-  
 end
