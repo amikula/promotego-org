ba5c4d90da8f57b4911b4139c473c246ca94c842
Alf Mikula <amikula@gmail.com>
2010-05-15 09:55:04 -0700

    Adding to_s method to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,30 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
+
+  def to_s
+    returning('') do |retval|
+      retval << "#{street_address}, " unless street_address.blank?
+      retval << city_state_zip
+    end
+  end
+
+  def city_state_zip
+    components = []
+    components << city unless city.blank?
+
+    state_zip = ''
+    if state.present?
+      state_zip << state
+      unless zip_code.blank?
+        state_zip << '  '
+        state_zip << zip_code.to_s
+      end
+    else
+      state_zip = zip_code.to_s
+    end
+
+    components << state_zip unless state_zip.blank?
+
+    components.join(', ')
+  end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -20,4 +20,34 @@ describe Address do
   it "should create a new instance given valid attributes" do
     Address.create!(@valid_attributes)
   end
+
+  describe :to_s do
+    it 'returns the zipcode when only zipcode is present' do
+      Address.new(:zip_code => 99999).to_s.should == '99999'
+    end
+
+    it 'returns the state when only state is present' do
+      Address.new(:state => 'CA').to_s.should == 'CA'
+    end
+
+    it 'returns city, state when only city and state are present' do
+      Address.new(:city => 'Los Angeles', :state => 'CA').to_s.should == 'Los Angeles, CA'
+    end
+
+    it 'returns address, zipcode when only address and zip code are present' do
+      Address.new(:street_address => '1313 Mockingbird Lane', :zip_code => 66666).to_s.
+        should == '1313 Mockingbird Lane, 66666'
+    end
+
+    it 'returns address, city, state when only address, city, and state are present' do
+      Address.new(:street_address => '1313 Mockingbird Lane', :city => 'Springfield', :state => 'XX').to_s.
+        should == '1313 Mockingbird Lane, Springfield, XX'
+    end
+
+    it 'returns address, city, state  zipcode' do
+      Address.new(:street_address => '1313 Mockingbird Lane', :city => 'Springfield',
+                  :state => 'XX', :zip_code => 66666).to_s.
+        should == '1313 Mockingbird Lane, Springfield, XX  66666'
+    end
+  end
 end
