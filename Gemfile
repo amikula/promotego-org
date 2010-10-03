0aad0ee4146bcd91e39a48da0258554a0206dc44
amikula <amikula@gmail.com>
2010-10-03 16:38:44 -0700

    Got tests passing with bundler 1.0

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'http://rubygems.org'
+
+gem "rails", "2.3.5"
+gem "rack", "1.0.1"
+gem "amatch"
+gem "haml", "2.0.9"
+gem "hpricot", "0.6.164"
+gem "nokogiri", "1.3.1"
+gem "hoe", "1.11.0"
+gem "chronic", "~> 0.2.3"
+gem "whenever", "0.4.0"
+gem "authlogic", "2.1.1"
+gem "geokit", "1.4.1"
+gem "alchemist", "0.1.1"
+gem "vlad", "~> 1.3.2"
+gem "rake", "~> 0.8.4"
+
+gem "mysql", "~> 2.8.0" unless ENV['HEROKU_SLUG']
+
+group :development do
+  gem "rdoc", ">= 2.4.3"
+  gem "fastercsv", "1.4.0"
+  gem "highline", "1.5.0"
+end
+
+group :test do
+  gem "spork", ">= 0.7.4"
+  gem "cucumber", "0.5.3"
+  gem "cucumber-rails", "0.2.2"
+  gem "rspec", "1.2.9"
+  gem "rspec-rails", "1.2.9"
+  gem "webrat", "0.6.0"
+  gem "database_cleaner", "0.2.3"
+end
--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,25 +1,20 @@
-# Run preinitializer from geminstaller
-require 'rubygems'
-require 'geminstaller'
+begin
+  require "rubygems"
+  require "bundler"
+rescue LoadError
+  raise "Could not load the bundler gem. Install it with `gem install bundler`."
+end
 
-args = ''
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
+  raise RuntimeError, "Your bundler version is too old for Rails 2.3." +
+   "Run `gem install bundler` to upgrade."
+end
 
-# Specify --geminstaller-output=all and --rubygems-output=all for maximum debug logging
-# args += ' --geminstaller-output=all --rubygems-output=all'
-
-# The 'exceptions' flag determines whether errors encountered while running GemInstaller
-# should raise exceptions (and abort Rails), or just return a nonzero return code
-args += " --exceptions"
-
-# This will use sudo by default on all non-windows platforms, but requires an entry in your
-# sudoers file to avoid having to type a password.  It can be omitted if you don't want to use sudo.
-# See http://geminstaller.rubyforge.org/documentation/documentation.html#dealing_with_sudo
-# Note that environment variables will NOT be passed via sudo!
-# args += " --sudo" unless RUBY_PLATFORM =~ /mswin/
-
-# The 'install' method will auto-install gems as specified by the args and config
-# GemInstaller.install(args)
-
-# The 'autogem' method will automatically add all gems in the GemInstaller config to your load path,
-# using the rubygems 'gem' method.  Note that only the *first* version of any given gem will be loaded.
-GemInstaller.autogem(args)
+begin
+  # Set up load paths for all bundled gems
+  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
+end
